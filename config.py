d08108acb9d4d529938c734a8016a5e61d341b43
nobara <fallyama2003@gmail.com>
2025-05-12 18:24:30 +0000

    Initialisation de l'app Flask avec create_app et config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os 
+
+class Config:
+    SECRET = os.environ.get('SECRET_KEY') or 'dev_secret_key'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt_secret_key'
+    DEBUG = True
+    # Plus tard DATABASE JWT CONFIGS ETC 
