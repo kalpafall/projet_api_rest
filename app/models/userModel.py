2f43c4ffcedfea5737a65519af9fb03cd19a967e
nobara <fallyama2003@gmail.com>
2025-05-14 13:16:49 +0000

    Création de create_user

--- app/models/userModel.py
+++ app/models/userModel.py
@@ -22,6 +22,20 @@ class UserModel:
         except Exception as e:
             print("Erreur lors de la récupération de l'utilisateur:", e)
             return None
+    def create_user(self, username, email, password_hash):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute("""
+                    INSERT INTO users (username, email, password_hash, is_active)
+                    VALUES (%s, %s, %s, TRUE)
+                    RETURNING id;
+                """, (username, email, password_hash))
+                self.conn.commit()
+                return cur.fetchone()[0]
+        except Exception as e:
+            print("Erreur lors de la création de l'utilisateur:", e)
+            self.conn.rollback()
+            return None
     def close(self):
         if self.conn:
             self.conn.close()
