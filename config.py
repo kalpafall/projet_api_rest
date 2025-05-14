b72efe248d5d72c21ef3cc86b5a5b5e4137d491a
nobara <fallyama2003@gmail.com>
2025-05-14 12:28:35 +0000

    encapusalation par une fonction get_connection

--- config.py
+++ config.py
@@ -1,20 +1,24 @@
-import os
 import psycopg2
+import os
 from dotenv import load_dotenv
 
+# Chargement des variables d'environnement depuis le fichier .env
 load_dotenv()
 
-conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    database=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    port=os.getenv("DB_PORT")
-)
-
-cur = conn.cursor()
-cur.execute("SELECT version();")
-version = cur.fetchone()
-print("PostgreSQL version:", version)
-cur.close()
-conn.close()
+def get_connection():
+    """
+    Crée et retourne une connexion PostgreSQL en utilisant psycopg2
+    Les paramètres sont récupérés à partir des variables d'environnement.
+    """
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT")
+        )
+        return conn
+    except Exception as e:
+        print("Erreur lors de la connexion à la base de données:", e)
+        return None
